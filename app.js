7ac03d7f623cb834f6bfd8b7adc5038701607cd5
ShashankBiplav <Biplavshashank7@gmail.com>
2020-06-01 12:44:59 +0530

    Funneling through different routes

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('In the middleware');
-    next();// allows the request to continue to the next middleware in line
-});
-
-app.use((req, res, next)=>{
+app.use('/add-product',(req, res, next)=>{
     console.log('In another middleware');
     //TODO: Send response
-    res.send('<h1> Hello from NodeJS via express');
+    res.send('<h1> Add products page</h1>');
+});
+
+
+app.use('/',(req, res, next)=>{
+    console.log('In another middleware');
+    //TODO: Send response
+    res.send('<h1> Hello from NodeJS via express</h1>');
 });
 
 // const server = http.createServer(app);
