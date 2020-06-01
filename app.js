7e712748c433b6d8926d27d174861ba80895b382
ShashankBiplav <Biplavshashank7@gmail.com>
2020-06-01 15:06:55 +0530

    Parsing incoming requests using body-parser

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 // const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));// this yields a middleware function to parse the incoming requests
 
 app.use('/add-product',(req, res, next)=>{
     console.log('In another middleware');
     //TODO: Send response
-    res.send('<h1> Add products page</h1>');
+    res.send('<form action ="/products" method ="POST"><input type = "text" name ="title"><button type ="submit">Add Product</button></form>');
 });
 
+app.use('/products',(req, res, next)=>{
+    console.log('In product middleware');
+    console.log(req.body);
+    
+   res.redirect('/');
+});
 
 app.use('/',(req, res, next)=>{
-    console.log('In another middleware');
+    console.log('In default middleware');
     //TODO: Send response
     res.send('<h1> Hello from NodeJS via express</h1>');
 });
