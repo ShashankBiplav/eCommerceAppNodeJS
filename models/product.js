eb23cd8909d03778ce6f7f80dc7bfded09cf83af
ShashankBiplav <Biplavshashank7@gmail.com>
2020-06-25 17:07:16 +0530

    Using the local filesystem to store the products

--- models/product.js
+++ models/product.js
@@ -1,13 +1,35 @@
-const products =[];
+const fs = require('fs');
+const path = require('path');
 
-module.exports= class Product{
-    constructor(title){
+const rootDir = require('../util/path.js');
+
+module.exports = class Product {
+    constructor(title) {
         this.title = title;
     }
-    save(){
-        products.push(this);
+    save() {
+        // products.push(this);
+        const p = path.join(rootDir,'data','products.json');
+        fs.readFile(p,(error, fileContent)=>{
+            let products =[];
+            if (!error) {
+                products =JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products),(err)=>{
+                console.log(err);
+            });
+        });
+            
     }
-    static fetchAll(){
-        return products;
+    static fetchAll() {
+        const p = path.join(rootDir,'data','products.json');
+        fs.readFile(p, (err, fileContent)=>{
+            if (err) {
+                return[];
+            }
+            return JSON.parse(fileContent);
+        });
+        // return products;
     }
 }
