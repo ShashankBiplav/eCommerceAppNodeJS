276bdc23cb672265f1754d80af1d8309ff2edf01
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-04 13:33:29 +0530

    Defined the mongoose version of User Schema

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error.js');
 
-// const User = require('./models/user.js');
+const User = require('./models/user.js');
 
 const app = express();
 
@@ -24,14 +24,14 @@ app.use(bodyParser.urlencoded({
 })); // yields a middleware function to parse the incoming requests
 app.use(express.static(path.join(__dirname, 'public'))); // path created to access public directory
 
-// app.use((req, res, next) => {
-//     User.findById("5efee95755793a24e8e4f6b9").then(user => {
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         next();
-//     }).catch(err => {
-//         console.log(err)
-//     });
-// });
+app.use((req, res, next) => {
+    User.findById("5f0037429d3526136280ac57").then(user => {
+        req.user = user;
+        next();
+    }).catch(err => {
+        console.log(err)
+    });
+});
 
 app.use('/admin', adminRoutes); //filtering the path via ->  /admin
 
@@ -40,7 +40,21 @@ app.use(shopRoutes);
 app.use(errorController.get404page);
 
 mongoose.connect('mongodb+srv://shashankbiplav:eCommerceNodeJS@ecommercenodejs.dcwx8.mongodb.net/eCommerceNodeJS?retryWrites=true&w=majority',
-// {useUnifiedTopology:true, useNewUrlParser: true}
-)
-    .then(app.listen(3000))
+        // {useUnifiedTopology:true, useNewUrlParser: true}
+    )
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Shashank Biplav',
+                    email: 'shashankbiplav@gmail.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        })
+        app.listen(3000);
+    })
     .catch(err => console.log(err));
