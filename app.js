95b51f9898d5840f053b08801da6f098ec0906b6
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-05 13:49:45 +0530

    Installed 'connect-mongodb-session' as production dependency and used it to store sessions on the server

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error.js');
 
 const User = require('./models/user.js');
 
+const MONGODB_URI = 'mongodb+srv://shashankbiplav:eCommerceNodeJS@ecommercenodejs.dcwx8.mongodb.net/eCommerceNodeJS?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -28,7 +35,8 @@ app.use(express.static(path.join(__dirname, 'public'))); // path created to acce
 app.use(session({
     secret: 'my secret',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
 }));
 
 app.use((req, res, next) => {
@@ -48,7 +56,7 @@ app.use(authRoutes);
 
 app.use(errorController.get404page);
 
-mongoose.connect('mongodb+srv://shashankbiplav:eCommerceNodeJS@ecommercenodejs.dcwx8.mongodb.net/eCommerceNodeJS?retryWrites=true&w=majority', {
+mongoose.connect(MONGODB_URI, {
         useUnifiedTopology: true,
         useNewUrlParser: true
     })
