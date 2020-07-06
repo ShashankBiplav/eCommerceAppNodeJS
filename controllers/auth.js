0877bd39189ccd3e4efc807b97641546b5fa2947
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-07 01:02:29 +0530

    Added 'bcryptjs' as a production dependency for password encryption

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user.js');
 
 exports.getLogin = (req, res, next) => {
@@ -22,7 +24,7 @@ exports.postLogin = (req, res, next) => {
         .then(user => {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            req.session.save((err)=>{
+            req.session.save((err) => {
                 console.log(err);
                 res.redirect('/');
             });
@@ -32,30 +34,37 @@ exports.postLogin = (req, res, next) => {
         });
 };
 
-exports.postSignup = (req,res, next) => {
+exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-    User.findOne({email: email})
-    .then((userDoc) => {
-        if (userDoc){
-            return res.redirect('/signup');
-        }
-        const user = new User({
-            email: email,
-            password: password,
-            cart: {items: []}
-        });
-        return user.save();
-    })
-    .then(result =>{
-        res.redirect('/login');
-    })
-    .catch(err => console.log(err));
+    User.findOne({
+            email: email
+        })
+        .then((userDoc) => {
+            if (userDoc) {
+                return res.redirect('/signup');
+            }
+            return bcrypt
+                .hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: {
+                            items: []
+                        }
+                    });
+                    return user.save();
+                }).then(result => {
+                    res.redirect('/login');
+                });
+        })
+        .catch(err => console.log(err));
 };
 
-exports.postLogout = (req,res,next) => {
-    req.session.destroy((err)=>{
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
         console.log(err);
         res.redirect('/');
     });
