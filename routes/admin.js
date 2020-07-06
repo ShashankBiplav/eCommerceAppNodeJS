5781225b839558b0232437d09c8d40c0a2bdbdee
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-07 01:35:11 +0530

    Used middleware for route protection

--- routes/admin.js
+++ routes/admin.js
@@ -6,22 +6,23 @@ const router = express.Router();
 
 const adminController = require('../controllers/admin.js');
 
+const isAuth = require('../middleware/is-auth.js');
 
 //   /admin/add-product ->GET request
-router.get('/add-product', adminController.getAddProductPage);
+router.get('/add-product', isAuth,  adminController.getAddProductPage);
 
 // /admin/products -> GET request
-router.get('/products', adminController.getProducts);
+router.get('/products',isAuth, adminController.getProducts);
 
 
 //   /admin/add-product ->POST request
-router.post('/add-product',adminController.postAddNewProduct);
+router.post('/add-product',isAuth,adminController.postAddNewProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProductPage);
+router.get('/edit-product/:productId',isAuth, adminController.getEditProductPage);
 
-router.post('/edit-product', adminController.postEditProduct);
+router.post('/edit-product',isAuth, adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/delete-product',isAuth, adminController.postDeleteProduct);
 
 module.exports = router; //  exporting all properties of router properties therefore this syntax
  
