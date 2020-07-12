bb1b16d0636c7030ad1f1e75522ac4d58094fd53
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-13 00:30:35 +0530

    Added logic to generate and save pdf using 'pdfkit'

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFdocument = require('pdfkit');
+
 const Product = require('../models/product.js');
 
 const Order = require('../models/order.js');
@@ -58,21 +60,21 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   // console.log(req.user.cart);
   req.user
-  .populate('cart.items.productId')
-  .execPopulate()
-  .then(user => {
-    const products = user.cart.items;
-    res.render('shop/cart', {
-      path: '/cart',
-      pageTitle: 'Your Cart',
-      products: products,
-    });
-  })
-  .catch(err => {
-    const error = new Error(err);
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products,
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
-  });
+    });
 };
 
 exports.postCart = (req, res, next) => {
@@ -82,7 +84,7 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then(result => {
-      console.log(result);
+      // console.log(result);
       res.redirect('/cart');
     })
     .catch(err => {
@@ -111,9 +113,14 @@ exports.postOrder = (req, res, next) => {
     .populate('cart.items.productId')
     .execPopulate()
     .then(user => {
-      console.log(user.cart.items);  
+      // console.log(user.cart.items);
       const products = user.cart.items.map(i => {
-        return { quantity: i.quantity, product: { ...i.productId._doc } };
+        return {
+          quantity: i.quantity,
+          product: {
+            ...i.productId._doc
+          }
+        };
       });
       const order = new Order({
         user: {
@@ -130,7 +137,7 @@ exports.postOrder = (req, res, next) => {
     .then(() => {
       res.redirect('/orders');
     })
-    .catch(err =>{
+    .catch(err => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -138,8 +145,10 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  Order.find({"user.userId": req.user._id})
-  .populate('products.product')
+  Order.find({
+      "user.userId": req.user._id
+    })
+    .populate('products.product')
     .then(orders => {
       // console.log(orders);
       res.render('shop/orders', {
@@ -164,30 +173,73 @@ exports.getCheckout = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  Order.findById(orderId)
-  .then(order=>{
-    if (!order) {
-      return next(new Error('No order found!'));
-    }
-    if (order.user.userId.toString() !== req.user._id.toString()) {
-      return next(new Error('Unauthorized'));
-    }
-    const invoiceName = 'invoice-'+ orderId + '.pdf';
-  const invoicePath = path.join('data', 'invoices', invoiceName);
-  // fs.readFile(invoicePath, (err, data)=>{
-  //   if (err) {
-  //    return next(err);
-  //   }
-  //   res.setHeader('Content-Type', 'application/pdf');
-  //   res.setHeader('Content-Disposition', 'inline; filename="'+ invoiceName+'"');
-  //   res.send(data);
-  // });
-  const file = fs.createReadStream(invoicePath);
-  res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'inline; filename="'+ invoiceName+'"');
-  file.pipe(res); //response is a writable stream 
-  })
-  .catch(err => {
-   next(err);
-  });
+  Order.findById(orderId).populate('products.product')
+    .then(order => {
+      if (!order) {
+        return next(new Error('No order found!'));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error('Unauthorized'));
+      }
+      const invoiceName = 'invoice-' + orderId + '.pdf';
+      const invoicePath = path.join('data', 'invoices', invoiceName);
+      const pdfDoc = new PDFdocument();
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.fontSize(26).text('Invoice', {
+          underline: true
+        });
+        pdfDoc.text('-----------------------');
+        let totalPrice = 0;
+
+        order.products.forEach(prod => {
+            totalPrice += prod.quantity * prod.product.price;
+            pdfDoc
+              .fontSize(14)
+              .text(
+                prod.product.title +
+                ' - ' +
+                prod.quantity +
+                ' x ' +
+                '$' +
+                prod.product.price
+              );
+          });
+          pdfDoc.text('---');
+          pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+          
+          pdfDoc.end();
+    })
+    .catch(err => {
+      next(err);
+    });
+
 };
+
+
+
+
+
+//
+// order.products.forEach(prod => {
+//   totalPrice += prod.quantity * prod.product.price;
+//   pdfDoc
+//     .fontSize(14)
+//     .text(
+//       prod.product.title +
+//       ' - ' +
+//       prod.quantity +
+//       ' x ' +
+//       '$' +
+//       prod.product.price
+//     );
+// });
+// pdfDoc.text('---');
+// pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+
+// pdfDoc.end();
+// })
+// 
