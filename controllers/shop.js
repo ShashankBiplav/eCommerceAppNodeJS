ab7a5c4944e6e11250739d261c6d198d7865c401
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-12 22:47:45 +0530

    Added a route and controller to handle authenticated invoice requests

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('../models/product.js');
 
 const Order = require('../models/order.js');
+const order = require('../models/order.js');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -155,5 +159,31 @@ exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
     pageTitle: 'Checkout',
+  });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  Order.findById(orderId)
+  .then(order=>{
+    if (!order) {
+      return next(new Error('No order found!'));
+    }
+    if (order.user.userId.toString() !== req.user._id.toString()) {
+      return next(new Error('Unauthorized'));
+    }
+    const invoiceName = 'invoice-'+ orderId + '.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName);
+  fs.readFile(invoicePath, (err, data)=>{
+    if (err) {
+     return next(err);
+    }
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="'+ invoiceName+'"');
+    res.send(data);
+  });
+  })
+  .catch(err => {
+   next(err);
   });
 };
