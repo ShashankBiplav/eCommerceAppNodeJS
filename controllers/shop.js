4ec5e94e01e0c016fbe0f0214c1b0cb4d2b1522b
ShashankBiplav <Biplavshashank7@gmail.com>
2020-07-13 23:06:17 +0530

    Added dynamic pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,15 +7,29 @@ const Product = require('../models/product.js');
 
 const Order = require('../models/order.js');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
-    .then(products => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
+  Product.find().countDocuments().then(numOfProducts=>{
+    totalItems = numOfProducts;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE);
+  })
+  .then(products => {
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'Products', // used in header
+        pageTitle: 'All Products', // used in header
         path: '/products', // to set active path
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page <totalItems,
+        hasPreviousPage: page>1,
+        nextPage: page+1,
+        previousPage: page-1,
+        lastPage: Math.ceil(totalItems/ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
@@ -43,16 +57,26 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  const page = +req.query.page || 1;
+  let totalItems;
 
-  Product.find()
-  .skip((page - 1) * ITEMS_PER_PAGE)
-  .limit(ITEMS_PER_PAGE)
-    .then(products => {
+  Product.find().countDocuments().then(numOfProducts=>{
+    totalItems = numOfProducts;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE);
+  })
+  .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop', // used in header
         path: '/', // to set active path
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page <totalItems,
+        hasPreviousPage: page>1,
+        nextPage: page+1,
+        previousPage: page-1,
+        lastPage: Math.ceil(totalItems/ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
