4d403991ca409ff556097ef06b3fcd5d2165858a
Shashank Biplav <Biplavshashank7@gmail.com>
2020-07-24 15:08:14 +0530

    Prepared for deployment on Heroku

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
 const helmet = require('helmet');
+const compression = require('compression');
 
 const errorController = require('./controllers/error.js');
 
@@ -49,6 +50,8 @@ const authRoutes = require('./routes/auth.js');
 const { use } = require('./routes/admin.js');
 
 app.use(helmet());
+
+app.use(compression());
 
 app.use(bodyParser.urlencoded({
     extended: false
